32724bc27ee7e135bdf9c0a1d4b3b3f133167a20
nika <nikalobjanidze014@gmail.com>
2025-01-20 17:54:05 +0400

    New AuthToken

--- Twilio/TwilioService.cs
+++ Twilio/TwilioService.cs
@@ -6,7 +6,7 @@ namespace Wolt_ConsoleApp.Twilio;
 internal class TwilioService
 {
     private readonly string _accountSid = "AC6050d2b967b8562ed0db05e3c377433f";
-    private readonly string _authToken = "14d2f4cc482071168991158d4eba73c1";
+    private readonly string _authToken = "a2495616ef5e6dfbdfcd7b0de4cc90b1";
     private readonly string _fromPhoneNumber = "+19783076324";
 
     public string SendVerificationCode(string toPhoneNumber)
