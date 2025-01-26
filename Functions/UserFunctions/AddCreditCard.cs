cbbe55caca6c118b6b881cf4324a94e056df0d29
nika <nikalobjanidze014@gmail.com>
2025-01-26 21:19:17 +0400

    Added Add Credit Card Function

--- Functions/UserFunctions/AddCreditCard.cs
+++ Functions/UserFunctions/AddCreditCard.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Globalization;
 using Wolt_ConsoleApp.Data;
 using Wolt_ConsoleApp.Models;
 namespace Wolt_ConsoleApp.Functions.UserFunctions
@@ -192,17 +193,173 @@ namespace Wolt_ConsoleApp.Functions.UserFunctions
                                         Clear();
                                         Console.WriteLine("Enter Credit Card Expiry Date (MM/dd/yyyy)");
                                         string UserCreditCardExpiryDate = Console.ReadLine();
+
+                                        DateTime currentDate = DateTime.Now;
+                                        if (string.IsNullOrWhiteSpace(UserCreditCardExpiryDate))
+                                        {
+                                            Clear();
+                                            Line();
+                                            Console.WriteLine("Credit Card Can not be Empty!");
+                                            Line();
+                                            Console.WriteLine("1. Try Again");
+                                            Console.WriteLine("2. Exit Adding CreditCard");
+                                            string CreditCardChoice = Console.ReadLine();
+                                            if (CreditCardChoice == "1")
+                                            {
+                                                continue;
+                                            }
+                                            else if (CreditCardChoice == "2")
+                                            {
+                                                Clear();
+                                                return;
+                                            }
+                                            else
+                                            {
+                                                InvalidChoice();
+                                                return;
+                                            }
+                                        }
+                                        else if (DateTime.TryParseExact(UserCreditCardExpiryDate, "MM/dd/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime parsedExpiryDate))
+                                        {                      
+                                            if (parsedExpiryDate < currentDate)
+                                            {
+                                                Clear();
+                                                Line();
+                                                Console.WriteLine("Credit Card Expiry Date cannot be in the past!");
+                                                Line();
+                                                Console.WriteLine("1. Try Again");
+                                                Console.WriteLine("2. Exit Adding CreditCard");
+                                                string CreditCardChoice = Console.ReadLine();
+                                                if (CreditCardChoice == "1")
+                                                {
+                                                    continue;
+                                                }
+                                                else if (CreditCardChoice == "2")
+                                                {
+                                                    Clear();
+                                                    return;
+                                                }
+                                                else
+                                                {
+                                                    InvalidChoice();
+                                                    return;
+                                                }
+                                            }                                      
+                                        }
                                         while (!IsValidCVV)
                                         {
                                             Clear();
                                             Console.WriteLine("Enter Credit Card CVV");
                                             string UserCreditCardCVV = Console.ReadLine();
+                                            if (string.IsNullOrWhiteSpace(UserCreditCardCVV))
+                                            {
+                                                Clear();
+                                                Line();
+                                                Console.WriteLine("Credit Card CVV cannot be empty!");
+                                                Line();
+                                                Console.WriteLine("1. Try Again");
+                                                Console.WriteLine("2. Exit Adding CreditCard");
+                                                string CreditCardChoice = Console.ReadLine();
+                                                if (CreditCardChoice == "1")
+                                                {
+                                                    continue;
+                                                }
+                                                else if (CreditCardChoice == "2")
+                                                {
+                                                    Clear();
+                                                    return;
+                                                }
+                                                else
+                                                {
+                                                    InvalidChoice();
+                                                    return;
+                                                }
+                                            }
+                                            if ((UserCreditCardCVV.Length == 3 || UserCreditCardCVV.Length == 4) && int.TryParse(UserCreditCardCVV, out _))
+                                            {
+                                                /// Continue to the next Loop. next is Balance
+                                            }
+                                            else
+                                            {
+                                                Clear();
+                                                Line();
+                                                Console.WriteLine("Credit Card CVV is Incorrect! Please enter 3 or 4 digits only.");
+                                                Line();
+                                                Console.WriteLine("1. Try Again");
+                                                Console.WriteLine("2. Exit Adding CreditCard");
+                                                string CreditCardChoice = Console.ReadLine();
+                                                if (CreditCardChoice == "1")
+                                                {
+                                                    continue;
+                                                }
+                                                else if (CreditCardChoice == "2")
+                                                {
+                                                    Clear();
+                                                    return;
+                                                }
+                                                else
+                                                {
+                                                    InvalidChoice();
+                                                    return;
+                                                }
+                                            }
                                             while (!IsValidBalance)
                                             {
                                                 Clear();
                                                 Console.WriteLine("Enter Credit Card Balance");
                                                 string UserCreditCardBalance = Console.ReadLine();
-
+                                                if (string.IsNullOrWhiteSpace(UserCreditCardBalance))
+                                                {
+                                                    Clear();
+                                                    Line();
+                                                    Console.WriteLine("Credit Card Balance cannot be empty!");
+                                                    Line();
+                                                    Console.WriteLine("1. Try Again");
+                                                    Console.WriteLine("2. Exit Adding CreditCard");
+                                                    string CreditCardChoice = Console.ReadLine();
+                                                    if (CreditCardChoice == "1")
+                                                    {
+                                                        continue;
+                                                    }
+                                                    else if (CreditCardChoice == "2")
+                                                    {
+                                                        Clear();
+                                                        return;
+                                                    }
+                                                    else
+                                                    {
+                                                        InvalidChoice();
+                                                        return;
+                                                    }
+                                                }
+                                                if (decimal.TryParse(UserCreditCardBalance, out decimal balance) && balance > 0)
+                                                {
+                                                    /// Continue to the next Step. Adding Credit Card
+                                                }
+                                                else
+                                                {
+                                                    Clear();
+                                                    Line();
+                                                    Console.WriteLine("Credit Card Balance must be a positive number and more than 0.");
+                                                    Line();
+                                                    Console.WriteLine("1. Try Again");
+                                                    Console.WriteLine("2. Exit Adding CreditCard");
+                                                    string CreditCardChoice = Console.ReadLine();
+                                                    if (CreditCardChoice == "1")
+                                                    {
+                                                        continue;
+                                                    }
+                                                    else if (CreditCardChoice == "2")
+                                                    {
+                                                        Clear();
+                                                        return;
+                                                    }
+                                                    else
+                                                    {
+                                                        InvalidChoice();
+                                                        return;
+                                                    }
+                                                }
                                                 CreditCard creditCard = new CreditCard
                                                 {
                                                     UserId = user.Id,
@@ -223,9 +380,10 @@ namespace Wolt_ConsoleApp.Functions.UserFunctions
                                                 IsValidExpiryDate = true;
                                                 IsValidCVV = true;
                                                 IsValidBalance = true;
-                                                ///
+                                                /// MAIN LOOPS
                                                 IsValidUserId = true;
                                                 IsAddedCreditCard = true;
+                                                /// MAIN LOOPS
                                             }
                                         }
                                     }
