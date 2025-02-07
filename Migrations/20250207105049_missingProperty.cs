ec2699194ce9aabf648fdd961a98cb51e155ca7b
nika <nikalobjanidze014@gmail.com>
2025-02-07 15:28:19 +0400

    Added Missing Property

---
+++ Migrations/20250207105049_missingProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wolt_ConsoleApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class missingProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "ProductQuantity",
+                table: "Products",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductQuantity",
+                table: "Products");
+        }
+    }
+}
